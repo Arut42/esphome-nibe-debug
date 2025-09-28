4866536fee2d1effdebf800667a7bdf57ba54d69
Arut42 <Arut42@users.noreply.github.com>
2025-09-28 13:42:17 +0200

    Update NibeGw.cpp
    dissable log of handleExpectedAck

--- components/nibegw/NibeGw.cpp
+++ components/nibegw/NibeGw.cpp
@@ -158,7 +158,7 @@ void NibeGw::handleDataReceived(byte b) {
 
 void NibeGw::handleExpectedAck(byte b) {
   buffer[index++] = b;
-  ESP_LOGV(TAG, "Recv: %02X", b);
+  //ESP_LOGV(TAG, "Recv: %02X", b);
   if (b == STARTBYTE_ACK || b == STARTBYTE_NACK) {
     /* Complete */
   } else if (b == STARTBYTE_MASTER) {
